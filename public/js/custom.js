e1659359594c8d07077bbcfe8bdbbe5b08454081
incubated-geek-cc <xuemanxu.cc@gmail.com>
2020-11-07 23:13:47 +0800

    Enable multi-selection of svg files

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,25 +1,50 @@
-var inputSvgCode=document.getElementById("inputSvgCode");
+function enablePopovers() {
+  var popoverTargets = document.querySelectorAll("[data-content]");
+
+  Array.from(popoverTargets).map(
+    popTarget => new BSN.Popover(popTarget, {
+      placement: "right",
+      animation: "show",
+      delay: 100,
+      dismissible: true,
+      trigger: "click"
+    })
+  );
+}
+enablePopovers();
+
 var convertBtn=document.getElementById("convertBtn");
 var importBtn=document.getElementById("importBtn");
 var loadSampleBtn=document.getElementById("loadSampleBtn");
+var saveSymbolsBtn=document.getElementById("saveSymbolsBtn");
 
 var results=[];
 
 function deepCopyObj(obj) {
-  var resultObj={};
-  for(var o in obj) {
+  let resultObj={};
+  for(let o in obj) {
     resultObj[o]=obj[o];
   }
   return resultObj;
 }
 
-function generateUid() {
-  var timestamp=new Date().getMilliseconds();
-  var r = Math.random()*16 | 0;
-  var v = (r & 0x3 | 0x8);
-  v=v.toString(16);
+function replaceAll(input,searchStr,replaceStr) {
+  input=input.split(searchStr);
+  input=input.join(replaceStr);
+  return input;
+}
 
-  return "icon-"+timestamp+"xxxxxxxx".replace(/[xy]/g, v);
+function generateUid() {
+  let result="";
+  let str=replaceAll(uuid.bin().toString(),",","-");
+  for(let i in str) {
+    if(i%2==0) {
+      result+="x";
+    } else {
+      result+=str[i];
+    }
+  }
+  return "icon-"+result;
 }
 
 function importFile() {
@@ -31,36 +56,45 @@ importBtn.onclick = function(e) {
 };
 
 importBtn.onchange = function(e) {
-  let fileName = e.target.value;
-  fileName = fileName.split("\\")[2];
-  let n = fileName.lastIndexOf(".");
-  fileName = fileName.substring(0,n);
-
-  if (!window.FileReader) {
-      alert("Your browser does not support HTML5 'FileReader' function required to open a file.");
-  } else {
-      var fileis = this.files[0];
-      var fileredr = new FileReader();
-      fileredr.onload = function (fle) {
-        var filecont = fle.target.result;
-        inputSvgCode.value = filecont;
-        if(filecont.trim() != "") {
-          convert();
-        }
-      };
-      fileredr.readAsText(fileis);
-  }
+  readMultiFiles(this.files);
 }; // new file input
 
-function convert() {
+function readMultiFiles(files) {
+ if (!window.FileReader) {
+    alert("Your browser does not support HTML5 'FileReader' function required to open a file.");
+  } else {
+    var fileReader = new FileReader();
+    function readFile(index) {
+      if(index >= files.length) {
+        return;
+      }
+      var file = files[index];
+      fileReader.onload = function(e) {
+        var filename=file.name;
+        var n = filename.lastIndexOf(".");
+        filename = filename.substring(0,n);
+
+        var filecontent=e.target.result;
+
+        convert(filename,filecontent);
+        readFile(index+1);
+      };
+      fileReader.readAsBinaryString(file);
+    }
+    readFile(0);
+  }
+};
+
+
+function convert(fileName,svgCode) {
   var url = "/api/convert-to-symbol";
   var paramsObj = {
-    svgData: inputSvgCode.value
+    svgData: svgCode
   };
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      elaborateResult(this.response);
+      elaborateResult(this.response,fileName);
     } else if(this.readyState == 4) {
       console.log("Error");
     }
@@ -70,28 +104,26 @@ function convert() {
   xhttp.send(JSON.stringify(paramsObj));
 }
 
-function elaborateResult(response) {
+function elaborateResult(response,fileName) {
   response=JSON.parse(response);
-  var symbol=response["symbol"];
-  var input=response["input"];
-
-  var uuid = generateUid();
-
-  var svg = input;
-  // CALL buildHTML function 
-  var data = {
+  let symbol=response["symbol"];
+  let input=response["input"];
+  let uuid = (fileName==null) ? generateUid() : fileName;
+  let svg = input;
+  
+  let data = {
     symbol: null,
     icon: null,
     htmlExample: null
   };
-  var rawSymbolCode=symbol;
+  let rawSymbolCode=symbol;
   rawSymbolCode=rawSymbolCode.replace("<symbol ", "<symbol id=\""+uuid+"\" ").replace(/<title>.*<\/title>\s/, "");
 
-  var icon = "<svg class=\"icon "+uuid+"\">";2
+  let icon = "<svg class=\"icon "+uuid+"\">";
   icon+="<use xlink:href=\"#"+uuid+"\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></use>";
   icon+="</svg>";
 
-  var symbolCode = rawSymbolCode.split("\n");
+  let symbolCode = rawSymbolCode.split("\n");
   symbolCode.shift();
   symbolCode.pop();
   symbolCode = symbolCode.join("\n ");
@@ -100,9 +132,9 @@ function elaborateResult(response) {
   data.icon = icon;
   data.symbolCode=symbolCode;
 
-  var compiledSamples = deepCopyObj(data);
+  let compiledSamples = deepCopyObj(data);
 
-  var now=new Date();
+  let now=new Date();
   let year=now.getFullYear();
   let month=now.getMonth();
   let days=now.getDate();
@@ -121,125 +153,114 @@ function elaborateResult(response) {
     symbol: compiledSamples.symbol,
     icon: compiledSamples.icon,
     symbolCode: compiledSamples.symbolCode,
-    timestamp: datestamp + " " + timing
+    timestamp: datestamp + " " + timing,
+    name: uuid
   });
 
-  var htmlStr="";
-  for(var r in results) {
-    var result=results[r];
-
-    var symbol=result.symbol;
-    symbol=symbol.replace("<svg width=\"0\" height=\"0\" style=\"position:absolute\">","").replace("</svg>","");
-    
-    htmlStr+="<svg class=\"svg_symbol_defs\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
-    htmlStr+=" <defs>";
-    htmlStr+=result.symbolCode;
-    htmlStr+=" </defs>";
-    htmlStr+="</svg>";
-
-    htmlStr+="<div class='panel col-sm-6'>";
-
-    htmlStr+="<div class='panel-heading small'>";
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-xs-6'><span class='btn btn-link'>Created at: " + result.timestamp +"</span></div>";
-    htmlStr+="<div class='col-xs-6 text-right'></div>";
-    htmlStr+="</div>"; // row
-    htmlStr+="</div>"; // heading
-
-    htmlStr+="<div class='panel-body'>";
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-sm-6 text-center'>";
-    htmlStr+="<label>";
-    htmlStr+="Svg (your input)";
-    htmlStr+="</label>";
-    htmlStr+="<div class='icon'>";
-    htmlStr+=result.svg;
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-
-    htmlStr+="<div class='col-sm-6 text-center'>";
-    htmlStr+="<label>";
-    htmlStr+="Svg (your converted to)";
-    htmlStr+="</label>";
-    htmlStr+="<div class='icon'>";
-    htmlStr+=result.icon;
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-    htmlStr+="</div>"; // row
-    htmlStr+="</div>"; // panel-body
-     
-    htmlStr+="<div class='panel-footer small'>";
-
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-sm-12 text-left'>";
-    htmlStr+="<span class='btn btn-link'>Code to be embedded: </span>";
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-sm-12'>";
-    htmlStr+="<textarea class='code' rows='7' disabled>";      
-    htmlStr+=result.symbolCode;
-    htmlStr+="</textarea>";
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-sm-12 text-left'>";
-    htmlStr+="<span class='btn btn-link'>Code to use in browser: </span>";
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-    htmlStr+="<div class='row'>";
-    htmlStr+="<div class='col-sm-12'>";
-    htmlStr+="<textarea class='code' rows='2' disabled>";
-    htmlStr+=result.icon;
-    htmlStr+="</textarea>";
-    htmlStr+="</div>";
-    htmlStr+="</div>";
-
-    htmlStr+="</div>"; // footer
-       
-    htmlStr+="</div>"; // panel
-
-    htmlStr+="</div>"; // col-sm-6
+  let htmlStr="";
+  for(let r in results) {
+    let result=results[r];
+    htmlStr+=renderThumbnail(result,r);
+    //htmlStr+=result.svg;
   }
-
   document.getElementById("results").innerHTML=htmlStr;
+
+  checkResultsArray();
+  enablePopovers();
 }
 
+function checkResultsArray() {
+  let actualLength=results.reduce((acc,cv) =>(cv)? acc+1 : acc, 0);
+
+  if(actualLength>0) {
+    saveSymbolsBtn["style"]["display"]="inline-block";
+  } else {
+    saveSymbolsBtn["style"]["display"]="none";
+  }
+}
+
+function deleteItemFromResults(index) {
+  document.getElementById("thumbnail_"+index).remove();
+  delete results[index];
+
+  checkResultsArray();
+}
+
+function renderThumbnail(result,index) {
+  let htmlStr="";
+  htmlStr+="<div id='thumbnail_"+index+"' class='col-xs-6 col-md-3'>";
+  htmlStr+="<svg class=\"svg_symbol_defs\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
+  htmlStr+="<defs>";
+  htmlStr+=result.symbolCode;
+  htmlStr+="</defs>";
+  htmlStr+="</svg>";
+  htmlStr+="<span class='btn btn-link' style='text-decoration:none;cursor:default'><small>Created at: " + result.timestamp +"</small>";
+
+  htmlStr+="<a class='thumbnail'>";
+  htmlStr+="<button class='btn btn-xs btn-danger rounded-circle pull-right' onclick='deleteItemFromResults("+index+")'>×</button>";
+  htmlStr+="<div class='icon'>";
+  htmlStr+=result.icon;
+  htmlStr+="</div>";
+  htmlStr+="<button type=\"button\" class=\"btn btn-link\" data-toggle=\"popover\" data-title=\"Code Preview: <kbd>" + result.name + "</kbd>\" data-dismissible=\"true\" data-placement=\"right\" data-content='<div class=\"row\">";
+  htmlStr+="<div class=\"col-sm-12\">";
+  htmlStr+="<h4><b><small>Symbol Definition(s)</small></b></h4>";
+  htmlStr+="<textarea class=\"code\" rows=\"5\" disabled>";
+  htmlStr+=result.symbolCode;
+  htmlStr+="</textarea>";
+  htmlStr+="</div>";
+  htmlStr+="</div>";
+  htmlStr+="<div class=\"row\">";
+  htmlStr+="<div class=\"col-sm-12\">";
+  htmlStr+="<h4><b><small>HTML(SVG(Use))</small></b></h4>";
+  htmlStr+="<textarea class=\"code\" rows=\"2\" disabled>";
+  htmlStr+=result.icon;
+  htmlStr+="</textarea>";
+  htmlStr+="</div>";
+  htmlStr+="</div>'>";
+  htmlStr+="<svg class=\"icon icon-code\"><use xlink:href=\"img/symbol-defs.svg#icon-code\"></use></svg> View Code";
+  htmlStr+="</button>";
+  htmlStr+="</div>";
+  htmlStr+="</a>";
+
+  return htmlStr;
+}
 
 function loadSample() {
   let str="";
   str+="<svg viewBox=\"0 0 32 32\">";
-  str+="<path d=\"M27.429 4v20q0 0.893-0.607 1.589t-1.536 1.080-1.848 0.571-1.723 0.188-1.723-0.188-1.848-0.571-1.536-1.080-0.607-1.589 0.607-1.589 1.536-1.080 1.848-0.571 1.723-0.188q1.875 0 3.429 0.696v-9.589l-13.714 4.232v12.661q0 0.893-0.607 1.589t-1.536 1.080-1.848 0.571-1.723 0.188-1.723-0.188-1.848-0.571-1.536-1.080-0.607-1.589 0.607-1.589 1.536-1.080 1.848-0.571 1.723-0.188q1.875 0 3.429 0.696v-17.268q0-0.554 0.339-1.009t0.875-0.634l14.857-4.571q0.214-0.071 0.5-0.071 0.714 0 1.214 0.5t0.5 1.214z\"></path>";
-  str+="</svg>";
-  inputSvgCode.value = str;
+  str+="\r\t<path d=\"M27.429 4v20q0 0.893-0.607 1.589t-1.536 1.080-1.848 0.571-1.723 0.188-1.723-0.188-1.848-0.571-1.536-1.080-0.607-1.589 0.607-1.589 1.536-1.080 1.848-0.571 1.723-0.188q1.875 0 3.429 0.696v-9.589l-13.714 4.232v12.661q0 0.893-0.607 1.589t-1.536 1.080-1.848 0.571-1.723 0.188-1.723-0.188-1.848-0.571-1.536-1.080-0.607-1.589 0.607-1.589 1.536-1.080 1.848-0.571 1.723-0.188q1.875 0 3.429 0.696v-17.268q0-0.554 0.339-1.009t0.875-0.634l14.857-4.571q0.214-0.071 0.5-0.071 0.714 0 1.214 0.5t0.5 1.214z\"></path>";
+  str+="\r</svg>";
 
-  convert();
+  convert("icon-music", str);
 }
 
-  
+function getSVGDefsToSave() {
+  var svg_defs_to_save="";
+  svg_defs_to_save+="<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
+  svg_defs_to_save+="\r\t<defs>";
 
+  for(var r in results) {
+    var result=results[r];
+    var symbolNode=result.symbolCode.replaceAll("xmlns=\"http://www.w3.org/2000/svg\"","");
+    svg_defs_to_save+="\r\t\t"+symbolNode;
+  }
 
-/*
-  $("#exportBtn").click(function(e) {
-    if (!window.Blob) {
-      alert("Your browser does not support HTML5 'Blob' function required to save a file.");
-    } else {
-      let textblob = new Blob([$("#geojson").text()], {
-          type: "text/plain"
-      });
-      let dwnlnk = document.createElement("a");
-      dwnlnk.download = "routes.geojson";
-      dwnlnk.innerHTML = "Download File";
-      if (window.webkitURL != null) {
-          dwnlnk.href = window.webkitURL.createObjectURL(textblob);
-      } else {
-          dwnlnk.href = window.URL.createObjectURL(textblob);
-          dwnlnk.onclick = destce;
-          dwnlnk.style.display = "none";
-          document.body.appendChild(dwnlnk);
-      }
-      dwnlnk.click();
-    }
-  });
-*/
+  svg_defs_to_save+="\r\t</defs>";
+  svg_defs_to_save+="\r</svg>";
+
+  if (!window.Blob) {
+    alert("Your browser does not support HTML5 'Blob' function required to save a file.");
+  } else {
+    let textblob = new Blob([svg_defs_to_save], {
+        type: "image/svg+xml"
+    });
+    let dwnlnk = document.createElement("a");
+    dwnlnk.download = "symbol-defs.svg";
+    dwnlnk.innerHTML = "Download File";
+    dwnlnk.href = window.URL.createObjectURL(textblob);
+    dwnlnk.style.display = "none";
+    document.body.appendChild(dwnlnk);
+
+    dwnlnk.click();
+  }
+}
